7cc08ed3aa2982cf858d4faf7fcad947b7590d55
Arnob Chowdhury <arnobchaudhury@gmail.com>
2021-04-09 13:21:28 +0530

    fixed make

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -137,8 +137,8 @@ int main(){
                     printf("last Name: %s\n", res.lastname);
                     printf("Aadhar Number: %s\n", res.aadhar_num);
                     printf("age: %d\n", res.age);
-                    printf("Height: %d\n", res.height);
-                    printf("Weight: %d\n", res.weight);
+                    printf("Height: %f\n", res.height);
+                    printf("Weight: %f\n", res.weight);
                     printf("Insurance Status: %s\n", (res.insurance == 1) ? "Insured" : "Not Insured");
                     printf("Vaccine code: %d\n", res.shot);
                     printf("Date of vaccination is: %s\n", res.date_of_vaccine);
