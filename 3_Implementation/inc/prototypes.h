dd547edb0ff99619d2fd929c26ad5e41d27de691
Arnob Chowdhury <arnobchaudhury@gmail.com>
2021-04-14 12:38:25 +0530

    Fixed Documentation

--- 3_Implementation/inc/prototypes.h
+++ 3_Implementation/inc/prototypes.h
@@ -35,10 +35,10 @@ typedef enum error_t{
 }error_t;
 
 /**
- * @brief Maps Colors to ints
+ * @brief Maps Vaccine codes to ints
  * 
  */
-typedef enum vaccine{ //color_b -> vaccine
+typedef enum vaccine{ 
     RED = 0,
     GREEN,
     YELLOW
@@ -70,12 +70,12 @@ typedef struct patient{
 
 
 /**
- * @brief Stores number of boxes with given color
+ * @brief Stores number of boxes with given vaccine
  * 
  */
 typedef struct vaccine_data{
-    int num; // Number of boxes with given color
-    vaccine shot; // given color
+    int num; // Number of boxes with given code
+    vaccine shot; // given code
 }vaccine_data;
 
 
@@ -84,100 +84,107 @@ typedef struct vaccine_data{
 /**
  * @brief Create a linked list object
  * @param[in] box *  Pointer to head node
- * @param[in] int  id of box
- * @param[in] float  length of box
- * @param[in] float  height of box
- * @param[in] float  weight of box
- * @param[in] float  breadth of box
- * @param[in] color_b color of box
- * @return box* 
+ * @param[in] int  id of patient
+ * @param[in] char firstname of patient
+ * @param[in] char lastname of patient
+ * @param[in] char Aadhar Number of patient
+ * @param[in] float height of patient
+ * @param[in] float weight of patient
+ * @param[in] int  age of patient
+ * @param[in] Insurance  insurance status 0 for No 1 for Yes of patient
+ * @param[in] Vaccine vaccine code of patient
+ * @return patient * 
  */
 patient *create_ll(patient *,int, char *, char *, char *, float, float,int,char *, insured, vaccine);
 
 /**
- * @brief display status of the boxes
- * @param[in]  box * Pointer to start node
+ * @brief display status of the patient records
+ * @param[in]  patient * Pointer to start node
  * @return error_t 
  */
 error_t display_ll(patient *);
 
 /**
- * @brief Add a new box at the end
+ * @brief Adds a node object at the end of the list
  * @param[in] box *  Pointer to head node
- * @param[in] int  id of box
- * @param[in] float  length of box
- * @param[in] float  height of box
- * @param[in] float  weight of box
- * @param[in] float  breadth of box
- * @param[in] color_b color of box
+ * @param[in] int  id of patient
+ * @param[in] char firstname of patient
+ * @param[in] char lastname of patient
+ * @param[in] char Aadhar Number of patient
+ * @param[in] float height of patient
+ * @param[in] float weight of patient
+ * @param[in] int  age of patient
+ * @param[in] Insurance  insurance status 0 for No 1 for Yes of patient
+ * @param[in] Vaccine vaccine code of patient
+ * @return error_t 
  */
 error_t insert_end(patient *,int, char *, char *, char *, float, float,int,char *, insured, vaccine);
+
 /**
- * @brief finds a box by Id
- * @param[in]  box * Pointer to start node
- * @param[out]  box * Pointer to result node
+ * @brief finds a patient record by Id
+ * @param[in]  patient * Pointer to start node
+ * @param[out]  patient * Pointer to result node
  * @param[in]  int id to be found
  * @return error_t 
  */
 error_t find_by_id(patient *, int, patient *, int *);
 
 /**
- * @brief counts all boxes of specific color
- * @param[in]  box * Pointer to start node
+ * @brief counts all patient by specific vaccine code
+ * @param[in]  patient * Pointer to start node
  * @param[out]  color_data * Pointer to result enum
- * @param[in] color_b color
+ * @param[in] Vaccine color
  * @return error_t 
  */
 error_t count_by_vaccine(patient *, vaccine_data *, vaccine); //Count by insurance
-//count_by_color -> count_by_vaccine
-
-/**
- * @brief Updates weight of a box by id
- * @param[in]  box * Pointer to start node
- * @param[in]  int   id to be found
- * @param[in]  box   weight to be updated
- * @return error_t 
- */
 
 /**
  * @brief Updates First Name of the patient
- * 
+ * @param[in]  patient * Pointer to start node
+ * @param[in]  int id to be updated
+ * @param[in]  int attribute to be updated
+ * @param[in]  int debug code 0 for production environment 1 for test environment
  * @return error_t 
  */
 error_t update_record(patient *, int, int, int); // last int is 0 -> prod , 1 -> testing
 
 /**
  * @brief deletes a patient record by id
- * @param[in]  box * Pointer to start node
- * @param[in]  int   id to be deleted
- * @return error_t 
+ * @param[in]  patient * Pointer to start node
+ * @param[in]  int id to be deleted
+ * @return patient *
  */
 patient *delete_patient(patient *, int);
 
 /**
  * @brief Deletes record from file
- * 
+ * @param[in]  int id to be deleted
+ * @param[in]  FILE ** Pointer to file to be updated
+ * @param[in]  FILE ** Pointer to temporary file 
+ * @return error_t 
  */
 error_t delete_rec_file(FILE **, FILE ** ,int);
 
 /**
  * @brief Updates Index file
- * 
+ * @param[in]  int id to be deleted
+ * @param[in]  FILE ** Pointer to file to be updated
+ * @param[in]  FILE ** Pointer to temporary file 
  * @return error_t 
  */
 error_t delete_index_file(FILE **, FILE ** ,int);
 
 /**
  * @brief Deletes all the nodes
- * @param[in]  box * Pointer to start node
+ * @param[in]  patient * Pointer to start node
  * 
  */
 void delete_all(patient *);
 
 /**
  * @brief Deletes one node from the beginning
- * @param[in]  box * Pointer to start node
- * @return box* 
+ * @param[in]  patient * Pointer to start node
+ * @return patient * 
  */
 patient *delete_beg(patient *);
 
@@ -193,19 +200,19 @@ error_t validate_id(FILE **fptr, int);
 /**
  * @brief this file opens and read ids stored.
  * 
- * @param addr 
- * @param mode 
- * @param fptr 
+ * @param char File name
+ * @param char File mode 
+ * @param File File pointer 
  * @return error_t 
  */
 error_t indexFile(char *addr, char *mode ,FILE **fptr);
 
 /**
- * @brief This is to store record of users
+ * @brief this file opens records file.
  * 
- * @param addr 
- * @param mode 
- * @param fptr 
+ * @param char File name
+ * @param char File mode 
+ * @param File File pointer 
  * @return error_t 
  */
 error_t openFile(char *addr, char *mode, FILE **fptr);
@@ -213,7 +220,9 @@ error_t openFile(char *addr, char *mode, FILE **fptr);
 /**
  * @brief To save each record in file
  * 
- * @param fptr 
+ * @param patient * Pointer to head node
+ * @param FILE * Pointer to file
+ * @param int * Signifies how data should be stored in file, 1 means overwrite file 0 means append to file
  * @return error_t 
  */
 error_t saveFile(patient *, FILE **, int *);
@@ -221,18 +230,25 @@ error_t saveFile(patient *, FILE **, int *);
 /**
  * @brief Reads file from file and prints
  * 
- * @param fptr 
+ * @param FILE file pointer
  * @return error_t 
  */
 error_t readFile(FILE **fptr);
 
 /**
  * @brief Loads data from file on program start up
- * 
+ * @param patient * Pointer to head node
+ * @param FILE file pointer
+ * @return patient *
+ */
+patient* loadData(patient *, FILE **);
+
+/**
+ * @brief During update operation, automatically gets called to update data in file if exists.
+ * @param FILE file pointer
+ * @param int id to be updated
  * @return error_t 
  */
-patient* loadData(patient *, FILE **); // Deprecated
-
 error_t update_data_file(FILE **, int);
 
 #endif
